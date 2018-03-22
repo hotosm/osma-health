f6e96819447377b483559a51e0e6af25ebeeef7e
Marc <marcfarra@gmail.com>
2018-03-22 17:01:26 -0400

    Add sass and jeet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">OSM Analytics for Health</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="col1">It works on the left!</div>
+        <div className="col2">It works on the right!</div>
       </div>
     );
   }
