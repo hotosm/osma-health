c8fa37fcd37d6af7092e1d73b3c6afb8ea207b39
Marc <marcfarra@gmail.com>
2018-04-06 17:35:24 -0400

    Refactor and add map

---
+++ src/components/ReportMap.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import mapboxgl from 'mapbox-gl';
+import centerOfMass from '@turf/center-of-mass';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q';
+
+export default class ReportMap extends Component {
+  componentDidMount() {
+    const aoi = this.props.aoi;
+    const center = centerOfMass(aoi).geometry.coordinates;
+
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/light-v9',
+      zoom: 11,
+      center
+    });
+
+    this.map.on('load', () => {
+      this.map.addSource('aoi', {
+        'type': 'geojson',
+        'data': aoi
+      });
+
+      this.map.addLayer({
+        'id': 'aoi-fill',
+        'type': 'fill',
+        'source': 'aoi',
+        'paint': {
+          'fill-color': '#088',
+          'fill-opacity': 0.1
+        }
+      });
+
+      this.map.addLayer({
+        'id': 'aoi-line',
+        'type': 'line',
+        'source': 'aoi',
+        'paint': {
+          'line-color': '#088',
+          'line-opacity': 1,
+          'line-dasharray': [4, 2],
+          'line-width': 2,
+        }
+      });
+    });
+  }
+  componentWillUnmount(){
+    this.map.remove();
+  }
+
+  render () {
+    const style = {
+      textAlign: 'left',
+      height: '100%'
+    };
+
+    return <div style={style} ref={el => this.mapContainer = el} />;
+  }
+}
---
+++ src/views/Report.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import ReportMap from '../components/ReportMap';
+
+class Report extends Component {
+  render() {
+    const aoi = this.props.aoi;
+
+    return (
+      <section className='page__body'>
+        <div className='map'>
+          <ReportMap aoi={aoi} />
+          <div className='report__panel'>
+            <div className='report__status report__status--good'>
+              <div className='inner'>
+                <p> AOI OSM Data Status: Good </p>
+              </div>
+            </div>
+            <div className='inner'>
+              <div className='report__actions'>
+                <p className='note'>Report last updated 3/12/18</p>
+                <button className='button button--small button--base-bounded'>Download Report</button>
+              </div>
+              <div className='report__header'>
+                <h1 className='report__title'>Ghanzi District</h1>
+                <ul className='report__meta'>
+                  <li>Botswana</li>
+                  <li>Est. Population 1,343</li>
+                </ul>
+              </div>
+              <div className='report__body'>
+                <div className='report__section'>
+                  <div className='report__section-header'>
+                    <h2 className='report__section-title'>Relative Completeness</h2>
+                  </div>
+                </div>
+                <div className='report__section'>
+                  <div className='report__section-header'>
+                    <h2 className='report__section-title'>Attribute Completeness</h2>
+                    <p className='note'>75% Completeness</p>
+                  </div>
+                  <div className='report__section-body'>
+                    <p><strong>15 </strong><small>OSM buildings in this AOI</small></p>
+                    <ul className='stat-list'>
+                      <li>15%<small>untagged closeways</small></li>
+                      <li>31%<small>residential buildings</small></li>
+                      <li>75%<small>residential buildings with roof and wall tags</small></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='report__section'>
+                  <div className='report__section-header'>
+                    <h2 className='report__section-title'>Temporal Accuracy</h2>
+                    <p className='note'>Last Edit: 3/2/2018 3:30pm</p>
+                  </div>
+                </div>
+                <div className='report__section'>
+                  <div className='report__section-header'>
+                    <h2 className='report__section-title'>Data Errors</h2>
+                  </div>
+                  <div className='report__section-body'>
+                    <ul className='stat-list'>
+                      <li>25<small>duplicated buildings</small></li>
+                      <li>31%<small>positioning errors</small></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section >
+    );
+  }
+}
+
+export default Report;
